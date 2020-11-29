c217adac19153501ca0c55ee5da5318cfb43fb7e
khvat_evgenii <xbat_@mail.ru>
2020-11-29 19:24:16 +0300

    Добавлен тест обратного преобразования из строки в JSON

--- common/src/test/java/jdev/dto/PointDTOTest.java
+++ common/src/test/java/jdev/dto/PointDTOTest.java
@@ -1,10 +1,15 @@
 package jdev.dto;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class PointDTOTest {
+    private String expected = "{\"lat\":56.0,\"lon\":74.0,\"autoId\":\"o567gfd\"}";
+    private String autoId = "o567gfd";
+
     @Test
     public void toJson() throws Exception {
         PointDTO point = new PointDTO();
@@ -12,5 +17,13 @@ public class PointDTOTest {
         point.setLon(74);
         point.setAutoId("o567gfd");
         assertTrue(point.toJson().contains("\"lat\":56"));
+        System.out.println(point.toJson());
+    }
+
+    @Test
+    public void decodeDTO() throws Exception{
+        ObjectMapper objectMapper = new ObjectMapper();
+        PointDTO point = objectMapper.readValue(expected, PointDTO.class);
+        assertEquals(autoId, point.getAutoId());
     }
 }
