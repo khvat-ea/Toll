2fcd9df76df1fcee09e64907c6609e893aa6a3b5
khvat_evgenii <xbat_@mail.ru>
2020-11-29 19:31:51 +0300

    Добавлена и протестирована метка времени

--- common/src/test/java/jdev/dto/PointDTOTest.java
+++ common/src/test/java/jdev/dto/PointDTOTest.java
@@ -7,8 +7,9 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class PointDTOTest {
-    private String expected = "{\"lat\":56.0,\"lon\":74.0,\"autoId\":\"o567gfd\"}";
+    private String expected = "{\"lat\":56.0,\"lon\":74.0,\"autoId\":\"o567gfd\",\"time\":1606667160082}";
     private String autoId = "o567gfd";
+    private long time = 1606667160082L;
 
     @Test
     public void toJson() throws Exception {
@@ -16,7 +17,9 @@ public class PointDTOTest {
         point.setLat(56);
         point.setLon(74);
         point.setAutoId("o567gfd");
+        point.setTime(System.currentTimeMillis());
         assertTrue(point.toJson().contains("\"lat\":56"));
+        assertTrue(point.toJson().contains("\"time\":"));
         System.out.println(point.toJson());
     }
 
@@ -25,5 +28,6 @@ public class PointDTOTest {
         ObjectMapper objectMapper = new ObjectMapper();
         PointDTO point = objectMapper.readValue(expected, PointDTO.class);
         assertEquals(autoId, point.getAutoId());
+        assertEquals(time, point.getTime());
     }
 }
