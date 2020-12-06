0b0bc7a1b665888cc2050540e6189890dfe4a71e
khvat_evgenii <xbat_@mail.ru>
2020-12-06 20:16:20 +0300

    Подготовлены шаблоны для сервисов trackerCore

---
+++ tracker-core/src/main/java/jdev/tracker/TrackerContext.java
@@ -0,0 +1,4 @@
+package jdev.tracker;
+
+public class TrackerContext {
+}
---
+++ tracker-core/src/main/java/jdev/tracker/TrackerMain.java
@@ -0,0 +1,8 @@
+package jdev.tracker;
+
+import jdev.dto.PointDTO;
+
+public class TrackerMain {
+    public static void main(String... args) throws Exception {
+    }
+}
